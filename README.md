107a30eb141e06c68bffd6cae28a7b1636c06bf0
alexmpereira <alexmpprog@gmail.com>
2022-05-01 19:25:24 -0300

    Estrutura inicial do projeto

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # concepts-solid-php
 Practical examples of SOLID in a PHP application
+
+#### Run Project
+**php -S localhost:8000**
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use App\Repository\TestDBConnection;
+
+header('Content-Type: text/html; charset=utf-8');
+
+// $id = $_GET['id'];//105 OR 1=1
+// $printData = new TestDBConnection($id);
+
